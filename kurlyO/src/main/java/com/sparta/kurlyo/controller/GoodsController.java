6de47587e54f9b7f12e7df8c28399573f683a7c5
SEONGHYUN PARK <sparklep519@gmail.com>
2023-03-04 00:25:00 +0900

    fix: MemberController login method(singup ->login)

--- kurlyO/src/main/java/com/sparta/kurlyo/controller/GoodsController.java
+++ kurlyO/src/main/java/com/sparta/kurlyo/controller/GoodsController.java
@@ -1,9 +1,14 @@
 package com.sparta.kurlyo.controller;
 
 import com.sparta.kurlyo.dto.GoodsResponseDto;
+import com.sparta.kurlyo.dto.ResponseDto;
+import com.sparta.kurlyo.security.UserDetailsImpl;
 import com.sparta.kurlyo.service.GoodsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -15,5 +20,10 @@ public class GoodsController {
     @GetMapping("/goods/{goodsId}")
     public GoodsResponseDto getDetails(@PathVariable long goodsId) {
         return goodsService.getDetails(goodsId);
+    }
+
+    @GetMapping("/categories")
+    public ResponseDto<List<GoodsResponseDto>> getCategoriesList(@RequestParam int page, int size, @RequestParam(defaultValue = "createdAt")String sortBy, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return GoodsService.getCategoriesList(page-1, size, sortBy);
     }
 }
--- kurlyO/src/main/java/com/sparta/kurlyo/service/GoodsService.java
+++ kurlyO/src/main/java/com/sparta/kurlyo/service/GoodsService.java
@@ -1,11 +1,19 @@
 package com.sparta.kurlyo.service;
 
 import com.sparta.kurlyo.dto.GoodsResponseDto;
+import com.sparta.kurlyo.dto.ResponseDto;
 import com.sparta.kurlyo.entity.Goods;
 import com.sparta.kurlyo.repository.GoodsRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,5 +29,18 @@ public class GoodsService {
         return goodsRepository.findById(goodsId).orElseThrow(
                 () -> new IllegalArgumentException("해당 상품이 존재하지 않습니다.")
         );
+    }
+
+    @Transactional
+    public ResponseDto<List<GoodsResponseDto>> getCategoriesList(int page, int size, String sortBy) {
+        Sort sort = Sort.by(Sort.Direction.DESC, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+        Page<Goods> goodsPage = goodsRepository.findAll(pageable);
+        List<Goods> goods = goodsPage.getContent();
+        List<GoodsResponseDto> goodsList = new ArrayList<>();
+        for(Goods goodsSet : goods){
+
+        }
+        return ResponseDto.success();
     }
 }
