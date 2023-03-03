c7a29f7377c5b080ca21deef76557285accdcebe
bestfarmer94 <sjasjaruddus@naver.com>
2023-03-03 18:00:46 +0900

    fix. import Auth

--- kurlyO/src/main/java/com/sparta/kurlyo/controller/GoodsController.java
+++ kurlyO/src/main/java/com/sparta/kurlyo/controller/GoodsController.java
@@ -1,4 +1,5 @@
 package com.sparta.kurlyo.controller;
 
 public class GoodsController {
+
 }
--- kurlyO/src/main/java/com/sparta/kurlyo/jwt/JwtUtil.java
+++ kurlyO/src/main/java/com/sparta/kurlyo/jwt/JwtUtil.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
