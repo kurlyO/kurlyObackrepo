496e54fa3f75fa08ccf410fe9e172e17958dfbe6
Jjuhui313 <hyacinth92928@gmail.com>
2023-03-03 15:41:34 +0900

    feat:기초세팅

---
+++ kurlyO/src/main/java/com/sparta/kurlyo/KurlyOApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.kurlyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurlyOApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KurlyOApplication.class, args);
+    }
+
+}
---
+++ kurlyO/src/test/java/com/sparta/kurlyo/KurlyOApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.kurlyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurlyOApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
