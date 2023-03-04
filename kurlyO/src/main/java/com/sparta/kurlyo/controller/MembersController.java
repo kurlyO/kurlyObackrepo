71d318bda65aa955c3f84bb3da0f330d0fcba877
Jjuhui313 <hyacinth92928@gmail.com>
2023-03-05 01:07:00 +0900

    test: CI/CD 테스트3-1

--- kurlyO/src/main/java/com/sparta/kurlyo/controller/MembersController.java
+++ kurlyO/src/main/java/com/sparta/kurlyo/controller/MembersController.java
@@ -20,6 +20,7 @@ import javax.validation.Valid;
 public class MembersController {
     private final MembersService membersService;
 
+
     @PostMapping("/signup")
     public ResponseEntity<Response> signup(@RequestBody @Valid SignupRequestDto signupRequestDto) {
         return membersService.signup(signupRequestDto);
