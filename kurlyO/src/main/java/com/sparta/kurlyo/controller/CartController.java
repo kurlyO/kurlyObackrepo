c0ec95f3b0cb86f11d8132de2c4e5597b3d50861
SEONGHYUN PARK <sparklep519@gmail.com>
2023-03-07 20:41:01 +0900

    style

--- kurlyO/src/main/java/com/sparta/kurlyo/controller/CartController.java
+++ kurlyO/src/main/java/com/sparta/kurlyo/controller/CartController.java
@@ -10,8 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
--- kurlyO/src/main/java/com/sparta/kurlyo/dto/GoodsListResponseDto.java
+++ kurlyO/src/main/java/com/sparta/kurlyo/dto/GoodsListResponseDto.java
@@ -1,22 +1,22 @@
-package com.sparta.kurlyo.dto;
-
-import com.sparta.kurlyo.entity.Category;
-import com.sparta.kurlyo.entity.Goods;
-import lombok.Builder;
-import lombok.Getter;
-
-import java.util.List;
-
-@Getter
-@Builder
-public class GoodsListResponseDto {
-    private List<GoodsResponseDto> goodsDtoList;
-    private List<CategoryDto> categoryDtoList;
-
-    public static GoodsListResponseDto of(List<GoodsResponseDto> goodsDtoList, List<CategoryDto> categoryDtoList) {
-        return GoodsListResponseDto.builder()
-                .goodsDtoList(goodsDtoList)
-                .categoryDtoList(categoryDtoList)
-                .build();
-    }
-}
+//package com.sparta.kurlyo.dto;
+//
+//import com.sparta.kurlyo.entity.Category;
+//import com.sparta.kurlyo.entity.Goods;
+//import lombok.Builder;
+//import lombok.Getter;
+//
+//import java.util.List;
+//
+//@Getter
+//@Builder
+//public class GoodsListResponseDto {
+//    private List<GoodsResponseDto> goodsDtoList;
+//    private List<CategoryDto> categoryDtoList;
+//
+//    public static GoodsListResponseDto of(List<GoodsResponseDto> goodsDtoList, List<CategoryDto> categoryDtoList) {
+//        return GoodsListResponseDto.builder()
+//                .goodsDtoList(goodsDtoList)
+//                .categoryDtoList(categoryDtoList)
+//                .build();
+//    }
+//}
