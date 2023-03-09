a7ce48bb3c7393bfa107cba073b7b670ccadcc12
SEONGHYUN PARK <sparklep519@gmail.com>
2023-03-09 12:38:47 +0900

    fix:멤버 편의 수정사항 재수정

--- kurlyO/src/main/java/com/sparta/kurlyo/service/MembersService.java
+++ kurlyO/src/main/java/com/sparta/kurlyo/service/MembersService.java
@@ -30,8 +30,7 @@ public class MembersService {
     @Transactional
     public ResponseEntity<Response> signup(SignupRequestDto signupRequestDto) {
         String accout = signupRequestDto.getAccount();
-//        String password = passwordEncoder.encode(signupRequestDto.getPassword());
-        String password = signupRequestDto.getPassword();
+        String password = passwordEncoder.encode(signupRequestDto.getPassword());
         String name = signupRequestDto.getName();
         String email = signupRequestDto.getEmail();
         String address = signupRequestDto.getAddress();
@@ -63,41 +62,44 @@ public class MembersService {
 
     }
 
-    //    @Transactional(readOnly = true)
-//    public ResponseEntity<Response> login(LoginRequestDto loginRequestDto, HttpServletResponse response) {
-//        String account = loginRequestDto.getAccount();
-//        String password = loginRequestDto.getPassword();
-//
-//        Optional<Members> member = membersRepository.findByAccount(account);
-//        if(!(member.isPresent() && passwordEncoder.matches(password, member.get().getPassword()))) {
-//            throw new CustomException(MEMBER_NOT_FOUND);
-//        }
-//        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(member.get().getName(), member.get().getRole()));
-//
-//        String token = jwtUtil.createToken(member.get().getName(), member.get().getRole());
-//        LoginResponseDto loginResponseDto = new LoginResponseDto(membersRepository.findByName(member.get().getName()).get().getName(), token);
-//        return new Response().toResponseEntity(LOGIN_SUCCESS, loginResponseDto);
-//
-//    }
-//
     @Transactional(readOnly = true)
     public ResponseEntity<Response> login(LoginRequestDto loginRequestDto, HttpServletResponse response) {
         String account = loginRequestDto.getAccount();
         String password = loginRequestDto.getPassword();
 
         Optional<Members> member = membersRepository.findByAccount(account);
-        if (!(member.isPresent() && password.equals(member.get().getPassword()))) {
+        if (!(member.isPresent() && passwordEncoder.matches(password, member.get().getPassword()))) {
             throw new CustomException(MEMBER_NOT_FOUND);
         }
-        String token = jwtUtil.createToken(member.get().getAccount(), member.get().getRole());
-        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, token);
-
         LoginResponseDto loginResponseDto = LoginResponseDto.of(member.get().getMemberName());
-        return Response.toResponseEntity(LOGIN_SUCCESS, loginResponseDto);
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(member.get().getMemberName(), member.get().getRole()));
+        return new Response().toResponseEntity(LOGIN_SUCCESS, loginResponseDto);
 
     }
 
-    public ResponseEntity<Response>accountCheck(String account) {
+    //    @Transactional(readOnly = true)
+//    public ResponseEntity<Response> login(LoginRequestDto loginRequestDto, HttpServletResponse response) {
+//        String account = loginRequestDto.getAccount();
+//        String password = loginRequestDto.getPassword();
+//
+//        Optional<Members> member = membersRepository.findByAccount(account);
+//        if (!(member.isPresent() && password.equals(member.get().getPassword()))) {
+//            throw new CustomException(MEMBER_NOT_FOUND);
+//        }
+//        String token = jwtUtil.createToken(member.get().getAccount(), member.get().getRole());
+//        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, token);
+//
+//        LoginResponseDto loginResponseDto = LoginResponseDto.of(member.get().getMemberName());
+//        return Response.toResponseEntity(LOGIN_SUCCESS, loginResponseDto);
+//
+//    }
+
+
+
+
+
+
+    public ResponseEntity<Response> accountCheck(String account) {
         if (membersRepository.findByAccount(account).isPresent()) {
             return Response.toAllExceptionResponseEntity(DUPLICATE_USER, account);
         }
